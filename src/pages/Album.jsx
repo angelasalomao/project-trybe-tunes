211d5b8fea8594df11b34157178423875bf82eb4
Angela Salomão <angelasalomaokl@gmail.com>
2022-05-29 11:01:21 -0300

    estilização da página albuns

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Proptypes from 'prop-types';
-import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
 import getMusics from '../services/musicsAPI';
 import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import Loading from './Loading';
 import './Album.css';
+import logo from '../images/Group_11.png'
+import LinksFooter from '../components/LinksFooter';
 
 class Album extends React.Component {
   constructor() {
@@ -54,17 +55,30 @@ class Album extends React.Component {
   render() {
     const { musics, nameArtist, nameAlbum, loading } = this.state;
     return (
-      <div data-testid="page-album">
-        <Header />
-        <h2 data-testid="artist-name">{ nameArtist }</h2>
-        <p data-testid="album-name">{ nameAlbum }</p>
-        { musics.filter((music) => music.kind === 'song')
-          .map((music, index) => (<MusicCard
-            key={ index }
-            music={ music }
-            isFavorite={ this.isMusicFavorite(music) }
-          />)) }
-        { loading && <Loading /> }
+      <div>
+      <div className="div-logo">
+        <img src={logo} alt="logo-trybe-tunes" className="logo-page-search" />
+      </div>
+      <div data-testid="page-album" className="album-page">
+          <p data-testid="artist-name" className="name-artist-title">
+            <b>Artist name:</b>
+            {' '}
+            {nameArtist}</p>
+          <p data-testid="album-name" className="name-album-title">
+            <b>Album name:</b>
+            {' '}
+            {nameAlbum}
+          </p>
+          </div>
+          <div className="card-music-container">
+          {musics.filter((music) => music.kind === 'song')
+            .map((music, index) => (<MusicCard
+              key={index}
+              music={music}
+              isFavorite={this.isMusicFavorite(music)} />))}
+          {loading && <Loading />}
+          </div>
+        <LinksFooter />
       </div>
     );
   }
